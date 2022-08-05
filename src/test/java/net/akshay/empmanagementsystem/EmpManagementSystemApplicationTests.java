850368aab642b793075d047eb09cd9be3049aea5
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2022-08-05 20:54:56 +0530

    Made package structure and Configured MySQL Database

---
+++ src/test/java/net/akshay/empmanagementsystem/EmpManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.akshay.empmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
