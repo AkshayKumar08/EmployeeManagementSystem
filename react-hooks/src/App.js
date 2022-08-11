c15d7bfabbbe621607924e407fe90669f29f9ca3
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2022-08-11 13:53:38 +0530

    Configured Routing in React

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -6,9 +7,16 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+        <Router>
+            <HeaderComponent/>
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element = {<ListEmployeeComponent/>}></Route>
+                        <Route path="/employees" element = {<ListEmployeeComponent/>}></Route>
+                    </Routes>
+                </div>
+            <FooterComponent/>
+        </Router>
     </div>
   );
 }
