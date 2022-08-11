77a2ebc58dc5cdddea43b7210fb57e2df4d722f3
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2022-08-11 13:38:11 +0530

    Created React Header and Footer in React App

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent/>
       <ListEmployeeComponent/>
+      <FooterComponent/>
     </div>
   );
 }
